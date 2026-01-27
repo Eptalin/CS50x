7521658f4001336b5a524519e7ad8237591bc9af
Eptalin <darrell.kiely@gmail.com>
2026-01-27 20:04:59 +0900

    improved mario-more

--- 1.C/mario-more/oldMario.c
+++ 1.C/mario-more/oldMario.c
@@ -15,28 +15,32 @@ int main(void)
     while (height < 1 || height > 8);
 
     // Print pyramids of that height
-    for (int row = 1; row <= height; row++)
+    for (int i = 0; i < height; i++)
     {
-        print_row(height - row, row);
+        print_row(height - i, i + 1);
     }
 }
-
 
 // Print a row of spaces and bricks
 void print_row(int spaces, int bricks)
 {
-    // Print left half spaces and bricks
-    for (int i = 0; i < spaces + bricks; i++)
+    // Print row of spaces
+    for (int j = spaces - 1; j > 0; j--)
     {
-        char c = (i < spaces) ? ' ' : '#';
-        printf("%c", c);
+        printf(" ");
     }
 
-    // Print 2-space divider
+    // Print row of bricks 1
+    for (int k = 0; k < bricks; k++)
+    {
+        printf("#");
+    }
+
+    // Print 2 spaces
     printf("  ");
 
-    // Print right half bricks
-    for (int i = 0; i < bricks; i++)
+    // Print row of bricks 2
+    for (int l = 0; l < bricks; l++)
     {
         printf("#");
     }
